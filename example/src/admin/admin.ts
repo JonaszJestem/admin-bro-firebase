f3620037d81c5918864aca50e1547fb563ce6928
JonaszJestem <jonaszwiacek@gmail.com>
2020-08-07 14:51:43 +0200

    fix: Typings

--- example/src/admin/admin.ts
+++ example/src/admin/admin.ts
@@ -6,7 +6,7 @@ import { createUserResource } from './resources/user/user.resource';
 import firebase from 'firebase';
 
 const setupAdmin = async (app: Express): Promise<void> => {
-  AdminBro.registerAdapter(FirestoreAdapter);
+  AdminBro.registerAdapter(FirestoreAdapter as any);
   const adminBro = new AdminBro({
     branding: {
       companyName: 'Firebase example',
