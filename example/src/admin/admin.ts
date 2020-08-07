eb0724046a6e2b0a8c48583f8a1d6c4b7f97b725
JonaszJestem <jonaszwiacek@gmail.com>
2020-08-07 14:31:57 +0200

    fix: Update example app

--- example/src/admin/admin.ts
+++ example/src/admin/admin.ts
@@ -1,12 +1,12 @@
 import AdminBroExpress from 'admin-bro-expressjs';
-import AdminBroFirebase from 'admin-bro-firebase';
+import { FirestoreAdapter } from 'admin-bro-firebase';
 import AdminBro from 'admin-bro';
 import { Express } from 'express';
 import { createUserResource } from './resources/user/user.resource';
 import firebase from 'firebase';
 
 const setupAdmin = async (app: Express): Promise<void> => {
-  AdminBro.registerAdapter(AdminBroFirebase);
+  AdminBro.registerAdapter(FirestoreAdapter);
   const adminBro = new AdminBro({
     branding: {
       companyName: 'Firebase example',
